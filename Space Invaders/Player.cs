6227dfe343bf31e11122344c144f5fdb2e87f086
saladucks <12WilliamsE>
2023-11-13 15:26:03 +0000

    Fun

---
+++ Space Invaders/Player.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace Space_Invaders
+{
+    public class Player : Sprite
+    {
+        private int lives = 3;
+
+        public Player() : base()
+        {
+        
+        } 
+
+        public Player(Texture2D spriteTexture, Vector2 spritePosition, Rectangle spriteBoundingBox, Color spriteColour) 
+            : base(spriteTexture, spritePosition, spriteBoundingBox, spriteColour)
+        {
+            _spritePosition = spritePosition;
+            _spriteBoundingBox = spriteBoundingBox;
+            _spriteColour = spriteColour;
+            _spriteTexture = spriteTexture;
+        }
+
+        public void FireBullet()
+        {
+            //Tell the bullet class we fired a bullet
+        }
+
+        public int Lives
+        {
+            get { return lives; }
+            set { lives = value; }
+        }
+    }
+}
---
+++ Space Invaders/Sprite.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Space_Invaders
+{
+    public class Sprite
+    {
+        protected Texture2D _spriteTexture;
+        protected Vector2 _spritePosition;
+        protected Rectangle _spriteBoundingBox;
+        protected Color _spriteColour;
+
+        public Sprite()
+        {
+
+        }
+
+        public Sprite(Texture2D spriteTexture, Vector2 spritePosition, Rectangle spriteBoundingBox, Color spriteColour)
+        {
+            _spriteBoundingBox = spriteBoundingBox;
+            _spriteTexture = spriteTexture;
+            _spritePosition = spritePosition;
+            _spriteColour = spriteColour;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_spriteTexture, _spritePosition, _spriteColour);
+        }
+
+        public Vector2 Position
+        {
+            get { return _spritePosition; }
+            set { _spritePosition = value; }
+        }
+
+        public Rectangle BoundingBox
+        {
+            get { return BoundingBox; }
+            set { BoundingBox = value; }
+        }
+
+
+    }
+}
