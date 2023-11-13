ca427b45e981962bbda0bffb88da41e352886763
saladucks <12WilliamsE>
2023-11-13 15:34:36 +0000

    Done

--- Space Invaders/Enemy.cs
+++ Space Invaders/Enemy.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace Space_Invaders
 {
-    internal class Enemy
+    public class Enemy : Sprite
     {
+        public Enemy() : base()
+        {
+
+        }
+
+        public Enemy(Texture2D spriteTexture, Vector2 spritePosition, Rectangle spriteBoundingBox, Color spriteColour)
+            : base(spriteTexture, spritePosition, spriteBoundingBox, spriteColour)
+        {
+            _spriteBoundingBox = spriteBoundingBox;
+            _spriteTexture = spriteTexture;
+            _spritePosition = spritePosition;
+            _spriteColour = spriteColour;
+        }
     }
 }
